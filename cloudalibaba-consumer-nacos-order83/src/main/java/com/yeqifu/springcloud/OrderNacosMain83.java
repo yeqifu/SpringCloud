2052336b4eb474d371a9118628cc6b7367e4a615
yeqifu <1784525940@qq.com>
2020-08-13 21:27:08 +0800

    Nacos作为服务注册中心

---
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/yeqifu/springcloud/OrderNacosMain83.java
@@ -0,0 +1,19 @@
+package com.yeqifu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author: 落亦-
+ * @Date: 2020/8/13 18:52
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderNacosMain83 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderNacosMain83.class,args);
+    }
+
+}
---
+++ cloudalibaba-provider-payment9001/src/main/java/com/yeqifu/springcloud/controller/PaymentController.java
@@ -0,0 +1,23 @@
+package com.yeqifu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: 落亦-
+ * @Date: 2020/8/13 18:28
+ */
+@RestController
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/nacos/{id}")
+    public String getPayment(@PathVariable("id") Integer id){
+        return "nacos registry,serverPort: "+serverPort+"\t"+id;
+    }
+
+}
